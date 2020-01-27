ca91ea71bbaab610dfeb7b67c0a85bf3a6d589e0
Lavan <kl980715@ohio.edu>
2020-01-26 19:21:21 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(kl980715_MIS4200.Startup))]
+namespace kl980715_MIS4200
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
