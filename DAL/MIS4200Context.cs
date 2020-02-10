18eca3e00e80e886d5ded1e1514c4f569abe6322
Kenneth Lavan <kl980715@ohio.edu>
2020-02-09 20:34:25 -0500

    Fixed

--- DAL/MIS4200Context.cs
+++ DAL/MIS4200Context.cs
@@ -13,6 +13,7 @@ namespace kl980715_MIS4200.DAL
         {
             // this method is a 'constructor' and is called when a new context is created
             // the base attribute says which connection string to use
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200Context, kl980715_MIS4200.Migrations.MISContext.Configuration>("DefaultConnection"));
         }
         // Include each object here. The value inside <> is the name of the class,
         // the value outside should generally be the plural of the class name
@@ -20,5 +21,11 @@ namespace kl980715_MIS4200.DAL
         public DbSet<Author> Author { get; set; }
         public DbSet<Book> Book { get; set; }
         public DbSet<Publisher> Publisher { get; set; }
+        // add this method - it will be used later
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
+
 }
